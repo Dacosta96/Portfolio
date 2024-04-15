c607204fd77b3e330f1f72dfa0c4fd0f14a83e59
DACOSTA96 <diego.ac9614@gmail.com>
2024-04-15 10:07:53 -0500

    efect color body

--- src/componentes/Home/index.js
+++ src/componentes/Home/index.js
@@ -1,11 +1,27 @@
-import React from "react";
+//import React from "react";
+import React, { useState } from 'react';
 import './styles.css';
 import foto1 from '../img/foto1.png'
 
 function Home(){
 
+
+    const cambiarColor = (event) => {
+        const x = event.clientX;
+        const y = event.clientY;
+        document.documentElement.style.setProperty('--position-x', x+'px');
+        document.documentElement.style.setProperty('--position-y', y+'px');
+
+        console.log(x);
+        console.log(y);
+      };
+    
+      
+     
+      
+
     return(
-        <div className="home">
+        <div className="home" onMouseMove={cambiarColor}>
             <div className="description">
                 <h2 className="hi">Hi, I´m </h2>
                 <h1 className="name">Diego Acosta</h1>
