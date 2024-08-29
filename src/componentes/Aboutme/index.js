5f98bfdd3709c660cf4393f6b597b91459f428d4
DACOSTA96 <diego.ac9614@gmail.com>
2024-08-29 15:39:21 -0500

    update portfolio

--- src/componentes/Aboutme/index.js
+++ src/componentes/Aboutme/index.js
@@ -1,97 +1,105 @@
 import React from "react";
-import './styles.css';
-import { ChevronDoubleRightIcon } from '@heroicons/react/24/solid';
-import logoReact from '../img/LogoReact.png';
-import logoJS from '../img/LogoJs.png';
-import logoHtml from '../img/LogoHtml.png';
-import logoCss from '../img/LogoCss.png';
-import logoTailw from '../img/LogoTaiw.png';
-import logoGit from '../img/Logogit.png';
-import logoGitHub from '../img/Logogithub.png';
-import logoVcode from '../img/LogoVcode.png';
+import "./styles.css";
+import { ChevronDoubleRightIcon } from "@heroicons/react/24/solid";
+import logoReact from "../img/LogoReact.png";
+import logoJS from "../img/LogoJs.png";
+import logoHtml from "../img/LogoHtml.png";
+import logoCss from "../img/LogoCss.png";
+import logoTailw from "../img/LogoTaiw.png";
+import logoGit from "../img/Logogit.png";
+import logoGitHub from "../img/Logogithub.png";
+import logoVcode from "../img/LogoVcode.png";
 
-function AboutMe(){
+function AboutMe() {
+  const skills = [
+    { name: "React.js", image: logoReact },
+    { name: "Javascript", image: logoJS },
+    { name: "Html", image: logoHtml },
+    { name: "Css", image: logoCss },
+    { name: "Tailwindcss", image: logoTailw },
+    { name: "Git", image: logoGit },
+    { name: "GitHub", image: logoGitHub },
+    { name: "Visual code", image: logoVcode },
+  ];
 
-    const skills=[
-        { name: 'React.js',
-          image:logoReact
-        },
-        { name: 'Javascript',
-          image:logoJS
-        },
-        { name: 'Html',
-          image:logoHtml
-        },
-        { name: 'Css',
-          image:logoCss
-        },
-        { name: 'Tailwindcss',
-          image:logoTailw
-        },
-        { name: 'Git',
-          image:logoGit
-        },
-        { name: 'GitHub',
-          image:logoGitHub
-        },
-        { name: 'Visual code',
-          image:logoVcode
-        },
+  const viewTags = (skills) => {
+    return (
+      <div className="tags">
+        {skills.map((skill, index) => (
+          <div key={index} className="icon-cards__content">
+            <img className="item" src={skill.image} alt={skill.name} />
+            <p className="textLogo">{skill.name}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
 
-    ]
-
-    const viewTags = (skills) =>{
-
-            return(
-                    <div className="tags">
-                        {skills.map((skill, index) => (
-                            <div key={index} className="icon-cards__content">
-                                <img className="item" src={skill.image} alt={skill.name} />
-                                <p className="textLogo">{skill.name}</p>
-                            </div>
-                        ))}
-                    </div>      
-        )
-    }
-
-    return(
-        <>
-        <div className="container">
-             <div className="container2">
-                <div className="aboutMe">ABOUT ME</div>
-                    <p className="contenido">
-                        I am an industrial engineer with experience in fullstack development and solid
-                        knowledge in process engineering. Self-taught and passionate about development
-                        and programming, in February 2024 I made the decision to leave my comfort zone 
-                        and enter this exciting world of development. My main goal is to become a high-level
-                        developer capable of providing solutions to any challenge that arises.
-                    </p>
-                    <div className="knowMore">know more<ChevronDoubleRightIcon className="icon"/></div>
-                    <div className="detail">
-                       <div className="detailName">
-                        <p><b>Name: </b><br /> Diego Fernando Acosta Salazar </p>
-                        <p><b>Birthday: </b><br /> 30 january </p>
-                        </div>
-                       <div className="education">
-                        <p><b>Passionate about: </b><br />Sports and Travel<br /><br /></p>
-                       <p className="titleEducation"><b>Education</b></p>
-                       <p>      
-                                <strong>Industrial Engineering</strong><br />
-                                Graduate - University Militar Nueva Granada 2017 - 2022<br /><br />
-                                <strong>Developer FullStack</strong><br />
-                                Autodidact - 2023 - at the moment
-                        </p>
-                       </div>
-
-                    </div>
+  return (
+    <>
+      <div className="container">
+        <div className="container2">
+          <div className="aboutMe">ABOUT ME</div>
+          <p className="contenido">
+            I am an industrial engineer with experience in full-stack
+            development and solid knowledge in process engineering. Self-taught
+            and passionate about development and programming, I took the leap to
+            transition into the exciting world of software development. My
+            background in engineering has equipped me with a strong analytical
+            mindset and problem-solving skills, which I now apply to create
+            efficient, scalable solutions in both front-end and back-end
+            development. I thrive in dynamic environments where continuous
+            learning and adaptability are key. My main goal is to become a
+            high-level developer capable of tackling complex challenges and
+            contributing innovative solutions that drive progress and
+            efficiency.
+          </p>
+          <div className="knowMore">
+            know more
+            <ChevronDoubleRightIcon className="icon" />
+          </div>
+          <div className="detail">
+            <div className="detailName">
+              <p>
+                <b>Name: </b>
+                <br /> Diego Fernando Acosta Salazar{" "}
+              </p>
+              <p>
+                <b>Birthday: </b>
+                <br /> 30 january{" "}
+              </p>
             </div>
-            <div className="skills">
-                <p>Skills</p>
-                 {viewTags(skills)}
+            <div className="education">
+              <p>
+                <b>Passionate about: </b>
+                <br />
+                Sports and Travel
+                <br />
+                <br />
+              </p>
+              <p className="titleEducation">
+                <b>Education</b>
+              </p>
+              <p>
+                <strong>Industrial Engineering</strong>
+                <br />
+                Graduate - University Militar Nueva Granada 2017 - 2022
+                <br />
+                <br />
+                <strong>Developer FullStack</strong>
+                <br />
+                Autodidact - 2023 - at the moment
+              </p>
             </div>
+          </div>
         </div>
-        </>
-    )
+        <div className="skills">
+          <p>Skills</p>
+          {viewTags(skills)}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export {AboutMe};
+export { AboutMe };
--- src/componentes/Portfolio/index.js
+++ src/componentes/Portfolio/index.js
@@ -1,84 +1,129 @@
 import React from "react";
-import './styles.css';
-import logoReact from '../img/LogoReact.png';
-import logoJS from '../img/LogoJs.png';
-import logoCss from '../img/LogoCss.png';
-import logoTailw from '../img/LogoTaiw.png';
-import logoHtml from '../img/LogoHtml.png';
-import imgProyectEcommerce from '../img/ProyectEcommerce.png';
-import imgProyectGolky from '../img/ProyectGolky.png';
+import "./styles.css";
+import logoReact from "../img/LogoReact.png";
+import logoJS from "../img/LogoJs.png";
+import logoCss from "../img/LogoCss.png";
+import logoMongo from "../img/LogoMongo.png";
+import logoTailw from "../img/LogoTaiw.png";
+import logoHtml from "../img/LogoHtml.png";
+import imgProyectEcommerce from "../img/ProyectEcommerce.png";
+import imgProyectGolky from "../img/ProyectGolky.png";
+import imgProyectCrm from "../img/ProjectCrm.jpg";
 
-function Portfolio(){
+function Portfolio() {
+  const handleOpenProyect1 = () => {
+    window.open("https://shopi-d.netlify.app/", "_blank");
+  };
 
-    const handleOpenProyect1 = () =>{
-        
-        window.open('https://shopi-d.netlify.app/', '_blank');
-    }
+  const handleCodeProyect1 = () => {
+    window.open("https://github.com/Dacosta96/Project_ecommerce", "_blank");
+  };
 
-    const handleCodeProyect1 =() =>{
-        window.open('https://github.com/Dacosta96/Project_ecommerce', '_blank');
-    }
+  const handleOpenProyect2 = () => {
+    window.open("https://www.golky.co/", "_blank");
+  };
 
-    const handleOpenProyect2 = () =>{
-        
-        window.open('https://www.golky.co/', '_blank');
-    }
+  const handleOpenProyect3 = () => {
+    alert("Sorry, this page is not public");
+  };
 
-    const handleCodeProyect2 =() =>{
-        alert('Sorry, this code is not public');
-    }
-    return(
-        <div className="portfolio">
-            <h1 className="title">Portfolio</h1>
-            <div className="proyects">
-                <div className="cardProyect">
-                <img className='imgProyect' src={imgProyectEcommerce} alt='img' />
-                <h3 className="titleProyect">Ecommerce Shopi-d</h3>
-                <p className="descriptionProyect">
-                    This is a frontend project developed entirely with React. 
-                    It provides an intuitive and attractive user interface for
-                    an e-commerce application.
-                </p>
-                <div className="logoTech">
-                    <img className='logoTech_logo' src={logoHtml} alt='img' />  
-                    <img className='logoTech_logo'  src={logoJS} alt='img' />
-                    <img className='logoTech_logo' src={logoReact} alt='img' />
-                    <img className='logoTech_logo' src={logoCss} alt='img' />
-                    <img className='logoTech_logo' src={logoTailw} alt='img' />
-                    
-                </div>
-                
-               <div className="buttons"><button onClick={handleOpenProyect1} className="buttonOpen">Open</button>
-               <button onClick={handleCodeProyect1} className="buttonCode">Code</button></div>
-                
-                </div>
-            
-                <div className="cardProyect">
-                <img className='imgProyect' src={imgProyectGolky} alt='img' />
-                <h3 className="titleProyect">Collaboration in Golky</h3>
-                <p className="descriptionProyect">
-                     I have collaborated with the creation of some interfaces 
-                     for the optimization and maintenance of the platform.
-                </p>
-                <div className="logoTech">
-                    <img className='logoTech_logo' src={logoHtml} alt='img' />  
-                    <img className='logoTech_logo'  src={logoJS} alt='img' />
-                    <img className='logoTech_logo' src={logoReact} alt='img' />
-                    <img className='logoTech_logo' src={logoCss} alt='img' />
-                    
-                </div>
-                
-               <div className="buttons"><button onClick={handleOpenProyect2} className="buttonOpen">Open</button>
-               <button onClick={handleCodeProyect2} className="buttonCode">Code</button></div>
-                
-                </div>
-            
-            </div>
-            <div className="buttonMoreProyect">
-            <button>More Proyects</button>
-            </div>
+  const handleCodeProyect2 = () => {
+    alert("Sorry, this code is not public");
+  };
+  return (
+    <div className="portfolio">
+      <h1 className="title">Portfolio</h1>
+      <div className="proyects">
+        <div className="cardProyect">
+          <img
+            className="imgProyect"
+            src={imgProyectCrm}
+            alt="img"
+            style={{ height: "180px", width: "80%" }}
+          />
+          <h3 className="titleProyect">CRM management system</h3>
+          <p className="descriptionProyect">
+            Development of a CRM for a U.S. company, transforming a manual
+            process into software that ensures the correct flow and handling of
+            information, with work being done on both the backend and frontend.
+          </p>
+          <div className="logoTech">
+            <img className="logoTech_logo" src={logoHtml} alt="img" />
+            <img className="logoTech_logo" src={logoJS} alt="img" />
+            <img className="logoTech_logo" src={logoReact} alt="img" />
+            <img
+              className="logoTech_logo"
+              src={logoMongo}
+              alt="MongoDB Logo"
+              style={{ width: "35px", height: "35px" }}
+            />
+          </div>
+
+          <div className="buttons">
+            <button onClick={handleOpenProyect3} className="buttonOpen">
+              Open
+            </button>
+            <button onClick={handleCodeProyect2} className="buttonCode">
+              Code
+            </button>
+          </div>
         </div>
-    )
+
+        <div className="cardProyect">
+          <img className="imgProyect" src={imgProyectGolky} alt="img" />
+          <h3 className="titleProyect">Collaboration in Golky</h3>
+          <p className="descriptionProyect">
+            I have collaborated with the creation of some interfaces for the
+            optimization and maintenance of the platform.
+          </p>
+          <div className="logoTech">
+            <img className="logoTech_logo" src={logoHtml} alt="img" />
+            <img className="logoTech_logo" src={logoJS} alt="img" />
+            <img className="logoTech_logo" src={logoReact} alt="img" />
+            <img className="logoTech_logo" src={logoCss} alt="img" />
+          </div>
+
+          <div className="buttons">
+            <button onClick={handleOpenProyect2} className="buttonOpen">
+              Open
+            </button>
+            <button onClick={handleCodeProyect2} className="buttonCode">
+              Code
+            </button>
+          </div>
+        </div>
+
+        <div className="cardProyect">
+          <img className="imgProyect" src={imgProyectEcommerce} alt="img" />
+          <h3 className="titleProyect">Ecommerce Shopi-d</h3>
+          <p className="descriptionProyect">
+            This is a frontend project developed entirely with React. It
+            provides an intuitive and attractive user interface for an
+            e-commerce application.
+          </p>
+          <div className="logoTech">
+            <img className="logoTech_logo" src={logoHtml} alt="img" />
+            <img className="logoTech_logo" src={logoJS} alt="img" />
+            <img className="logoTech_logo" src={logoReact} alt="img" />
+            <img className="logoTech_logo" src={logoCss} alt="img" />
+            <img className="logoTech_logo" src={logoTailw} alt="img" />
+          </div>
+
+          <div className="buttons">
+            <button onClick={handleOpenProyect1} className="buttonOpen">
+              Open
+            </button>
+            <button onClick={handleCodeProyect1} className="buttonCode">
+              Code
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="buttonMoreProyect">
+        <button>More Proyects</button>
+      </div>
+    </div>
+  );
 }
 
-export {Portfolio};
+export { Portfolio };
